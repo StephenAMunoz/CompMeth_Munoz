4b034440c0cee9e15da68d7fb8db3c19ec6ab29f
Steve <smunoz@gradcenter.cuny.edu>
2017-09-07 12:36:53 -0400

    this is a commit message

---
+++ Day3.py
@@ -0,0 +1,8 @@
+https://github.com/StephenAMunoz/CompMeth_Munoz.git
+
+make empty directory
+
+git clone repository
+
+send professor maller the repository link through email!!
+in repository, 2 programs written doing 2 of the exercises
