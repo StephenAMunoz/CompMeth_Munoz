13b63ed68dc70a6f22871ad5a125b7b09447b1dc
Steve <smunoz@gradcenter.cuny.edu>
2017-09-11 18:50:02 -0400

    removing unwanted files

--- Day3.py
+++
@@ -1,8 +0,0 @@
-https://github.com/StephenAMunoz/CompMeth_Munoz.git
-
-make empty directory
-
-git clone repository
-
-send professor maller the repository link through email!!
-in repository, 2 programs written doing 2 of the exercises
--- README.md
+++
@@ -1,2 +0,0 @@
-# CompMeth_Munoz
-Repository for Fall 2017 class Computational Methods with Prof. Maller
